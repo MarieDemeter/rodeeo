ead60c470820a11bde1ecdc19aea1e8851c85e66
MarieDemeter <mdemeter.ducrot@gmail.com>
2023-01-17 13:23:57 +0100

    style: :sparkles: Implementation of bootstrap-vue and custom css file

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -8,6 +8,11 @@
 import { loadFonts } from './webfontloader'
 import vuetify from './vuetify'
 import router from '../router'
+import 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '@/styles/custom.css'
 
 export function registerPlugins (app) {
   loadFonts()
