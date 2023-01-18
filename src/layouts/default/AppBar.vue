b0a32ebb22e7594f7ab76ff5d1cabc2cc7722461
MarieDemeter <mdemeter.ducrot@gmail.com>
2023-01-18 13:35:23 +0100

    feat: :art: Change the app-bar to navigate on the site

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,52 @@
 <template>
-  <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-4" />
+    <v-app-bar elevate-on-scroll color="light-cream" class="text-cream">
+        <v-app-bar-title> Marie-Déméter DUCROT </v-app-bar-title>
+        <v-menu open-on-hover>
+            <template v-slot:activator="{ props }">
+                <v-btn color="cream" v-bind="props" class="text-uppercase">
+                    Articles
+                </v-btn>
+            </template>
 
-      Base Preset
-    </v-app-bar-title>
-  </v-app-bar>
+            <v-list>
+                <v-list-item v-for="item in articles" :key="item">
+                    <router-link :to="{ name: item.route }" class="btn text-cream text-uppercase">{{item.title}}</router-link>
+                </v-list-item>
+            </v-list>
+            
+        </v-menu>
+        <v-menu open-on-hover>
+            <template v-slot:activator="{ props }">
+                <v-btn color="cream" v-bind="props" class="text-uppercase">
+                    Utilisateurs
+                </v-btn>
+            </template>
+
+            <v-list>
+                <v-list-item v-for="item in articles" :key="item">
+                    <router-link :to="{ name: 'Posts' }" class="btn text-uppercase text-cream">{{item.title}}</router-link>
+                </v-list-item>
+            </v-list>
+            
+        </v-menu>
+    </v-app-bar>
 </template>
 
-<script setup>
-  //
+<script>
+export default {
+    data() {
+        return {
+            articles: [
+                {
+                    title: "Tous les articles",
+                    route: "Posts"
+                },
+                {
+                    title: "Ecrire un article",
+                    route: "AddPost"
+                },
+            ]
+        }
+    },
+}
 </script>
